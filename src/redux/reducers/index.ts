b13d06c6f3ef235b73904580dd0116ac96c10888
aanikid <lotfiaanikid8@gmail.com>
2022-05-27 01:49:17 +0200

    ♻️ remove death code

--- src/redux/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "@reduxjs/toolkit";
-import TennisReducer from "./TennisReducer";
-
-export const rootReducer = combineReducers({
-    tennis: TennisReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>
